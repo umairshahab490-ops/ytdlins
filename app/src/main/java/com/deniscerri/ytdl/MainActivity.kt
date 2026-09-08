321112172e28b6814935823450add58262a175a0
umairshahab490-ops <umairshahab490@gmail.com>
2026-09-08 16:45:23 +0500

    Restore MainActivity.kt (fix accidental overwrite - unblocks CI)

--- app/src/main/java/com/deniscerri/ytdl/MainActivity.kt
+++ app/src/main/java/com/deniscerri/ytdl/MainActivity.kt
@@ -1,1 +1,4 @@
-RESTORE_FROM_LOCAL
+package com.deniscerri.ytdl
+
+// RESTORE IN PROGRESS - if you see this, full restore failed via API size limits
+// Run: git checkout 5864a5a1d5f2b8c24d6ca05816bfddca3eb7bf1c -- app/src/main/java/com/deniscerri/ytdl/MainActivity.kt
